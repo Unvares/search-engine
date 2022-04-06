f93d7e60f6c616b6a8d3f48d2b5a1b1adbce5a5e
unvares <okmanis8@gmail.com>
2022-04-06 20:21:13 +0300

    feat: add ContentManager and paginator

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import './assets/javascript/server.js'
 import './assets/javascript/init.js';
-import './assets/javascript/modules/searchPanelManager.js';
+import './assets/javascript/modules/contentManager.js';
 import './assets/javascript/loadModules.js';
