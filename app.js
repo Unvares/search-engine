dece9a82b96511eeed48ee0775e649d4d499d81b
unvares <okmanis8@gmail.com>
2022-04-19 13:30:36 +0300

    feat: refactor modules' exports to classes

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-import './assets/javascript/modules/contentManager.js';
+import ContentManager from './assets/javascript/modules/ContentManager.js';
+
+const contentManagerData = {
+  searchPanel: document.querySelector('.js-search-panel'),
+  searchLine: document.querySelector('.js-search-line'),
+  paginator: document.querySelector('.js-paginator'),
+  previousButton: document.querySelector('.js-previous-page'),
+  nextButton: document.querySelector('.js-next-page'),
+  screenCapacity: 6,
+};
+const contentManager = new ContentManager(contentManagerData);
